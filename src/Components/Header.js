40ea721b13c789deff5220307b59ed0fb4ad817c
“gchandru283” <gchandru283@gmail.com>
2023-10-19 07:48:57 +0530

    Initial Commit 2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,10 @@
 import React from "react";
 function Header(){
 return(
-    <div className="text-white font-mono">Hello World!</div>
+    <div className="text-red-500 text-center font-mono fixed w-full">
+        Chandru Here!!
+        
+    </div>
 );
 }
 

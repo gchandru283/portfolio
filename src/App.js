dd9a9c8246279b8b5513e92e94d67b55ae52985f
“gchandru283” <gchandru283@gmail.com>
2023-10-29 10:10:57 +0530

    resume !

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./Components/Header";
 
 function App() {
+  const resumeUrl = process.env.PUBLIC_URL + '/files/Chandru.pdf';
   return (
     <div className="bg-black h-screen">
       <Header/>
+      <div className="text-white px-5 font-mono fixed w-full text-center">
+      Download my resume - <a className="text-blue-400" href={resumeUrl} download="resume.pdf">here!!</a>
+      </div>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,8 @@
 import React from "react";
 function Header(){
 return(
-    <div className="text-red-500 text-center font-mono fixed w-full">
-        Chandru Here!!
-        
+    <div className="h-5">
+            
     </div>
 );
 }

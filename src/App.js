8b07cb99f715d48fa0e5c02a3001ffa0e1c02ca1
“gchandru283” <gchandru283@gmail.com>
2024-08-11 10:44:24 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
+import { useEffect, useState } from "react";
 import Header from "./Components/Header";
 
 function App() {
+  const [visitCount, setVisitCount] = useState(0);
+  const resumeUrl = process.env.PUBLIC_URL + '/files/Chandru.pdf';
+  const driveLink = "https://drive.google.com/file/d/1MPzsmegnK0Na9ZXuxbpapP8X/view";
 
-  const driveLink = "https://drive.google.com/uc?id=1MPzsmegnK0Na9ZXuxbpapP8XAwxztNEC";
+  useEffect(() => {
+    // Check if the user has visited before using localStorage
+    const hasVisited = localStorage.getItem('hasVisited');
+    if (!hasVisited) {
+      // Increment the visit count in localStorage if the user hasn't visited before
+      const currentCount = parseInt(localStorage.getItem('visitCount') || '0', 10);
+      const newCount = currentCount + 1;
+      localStorage.setItem('visitCount', newCount.toString());
+      localStorage.setItem('hasVisited', 'true');
+      setVisitCount(newCount);
+    } else {
+      // If the user has visited before, just retrieve the current visit count
+      const currentCount = parseInt(localStorage.getItem('visitCount'), 10);
+      setVisitCount(currentCount);
+    }
+  }, []);
+
   return (
     <div className="bg-black h-screen">
-      <Header/>
+      <Header />
       <div className="text-white px-5 font-mono fixed w-full text-center">
-      Download my resume - <a className="text-blue-400" href={driveLink} target="_blank" rel="noreferrer noopener" download="Chandru resume.pdf">here!!</a>
+        Download my resume - <a className="text-blue-400" href={driveLink}>here!!</a>
+        <div>
+          The page has been visited {visitCount} times.
+        </div>
       </div>
     </div>
   );
